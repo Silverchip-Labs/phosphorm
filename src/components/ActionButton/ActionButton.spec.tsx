9cebd3d67b2dc09bb9a31b04b28d94d5106627b6
Rob Fairclough <rob@silverchip.com>
2023-05-17 11:39:08 +0100

    add setup tests file

--- src/components/ActionButton/ActionButton.spec.tsx
+++ src/components/ActionButton/ActionButton.spec.tsx
@@ -1,6 +1,3 @@
-// todo setup-tests.js
-import '@testing-library/jest-dom/extend-expect';
-
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import ActionButton from './ActionButton';
--- src/components/EmailInput/EmailInput.spec.tsx
+++ src/components/EmailInput/EmailInput.spec.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom/extend-expect';
 import React, { useState } from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import EmailInput, { EmailInputProps } from './EmailInput';
