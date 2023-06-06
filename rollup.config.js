55416b4fa879daeba428a80e7b077b604435b812
Rob Fairclough <rob@silverchip.com>
2023-06-06 08:26:34 +0100

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ export default [
         plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()],
     },
     {
-        input: 'dist/esm/types/index.d.ts',
+        input: 'dist/esm/index.d.ts',
         output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         plugins: [dts()],
         external: [/\.(css|less|scss)$/],
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
+export * from './types';
+export * from './components';
 export { default as useForm } from './hooks/useForm';
 export { default as useFieldValidation } from './hooks/useFieldValidation';
-export * from './components';
-export * from './types';
